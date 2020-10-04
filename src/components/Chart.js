582f1168884342a781c51252ea9ab7c350b86a8d
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2020-10-04 17:40:09 -0500

    Added card to principal page Chart.js

---
+++ src/components/Chart.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+const ChartP = () => {
+    
+    return(
+        <div className="row justify-content-center">
+            <div className="col-md-10">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">Método Gráfico</h2>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default ChartP;

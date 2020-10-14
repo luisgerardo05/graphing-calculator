700267db69cde9783bb804b918de51d3297f040f
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2020-10-14 14:31:38 -0500

    Added solution state and modified display solution

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,7 +4,11 @@ import Axios from 'axios';
 
 class Chart extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      solution: {x: 0, y: 0, z: 0, type: ''},
+      error: false
+    };
   }
 
   render () {
@@ -12,7 +16,7 @@ class Chart extends Component {
 
     let logicJS = async (board) => {
       board.suspendUpdate();
-      var obj = board.create("line", [[parseFloat(objFunc.x), 0], [0, parseFloat(objFunc.y)]], {strokeColor: "#42f551"});
+      // var obj = board.create("line", [[parseFloat(objFunc.x), 0], [0, parseFloat(objFunc.y)]], {strokeColor: "#42f551"});
       
       restData.forEach((element) => {
         var line = board.create("line", [-1 * parseFloat(element.c), parseFloat(element.x1), parseFloat(element.x2)], {strokeColor: "#000000"});
@@ -51,36 +55,40 @@ class Chart extends Component {
           }
         );
 
-        /**Graficar la solucion y el punto */
         let resultado = data.result;
         let xfinal = data.resultX;
         let yfinal = data.resultY;
 
-        /**Redondear a dos decimales */
         resultado = Math.round(100 * resultado) / 100;
         xfinal = Math.round(100 * xfinal) / 100;
         yfinal = Math.round(100 * yfinal) / 100;
-        /**Linea final */
+
+        this.setState({solution: {
+          x: xfinal, 
+          y: yfinal, 
+          type: objFunc.type,
+          z: (objFunc.x * xfinal) + (objFunc.y * yfinal)
+        }});
+
         let finalLine = board.create(
           "line",
           [-1 * resultado, parseFloat(objFunc.x), parseFloat(objFunc.y)],
           {
-            strokeColor: "#97266d",
+            strokeColor: "#42f551",
           }
         );
-        /**Punto de solucion */
+        
         let puntoSolucion = board.create("point", [xfinal, yfinal]);
 
       } catch (error) {
         if (error.response) {
+          this.setState({error: true});
           console.log("No hay solucion");
         } else {
           console.log("Ocurrió un error de comunicación, intentelo de nuevo");
         }
       }
 
-      console.log(objective);
-      console.log(aux);
       board.unsuspendUpdate();
     }
 
@@ -96,8 +104,11 @@ class Chart extends Component {
 
         <ul className="list-group">
           <li className="list-group-item">Región de soluciones factibles</li>
-          <li className="list-group-item list-group-item-success">Función objetivo</li>
-          <li className="list-group-item list-group-item-danger">Solución<br/>x = <br/>y = </li>
+          <li className="list-group-item list-group-item-success">
+            {this.state.error ? <p>No hay solucion</p>
+                              : <p>Solución<br/>{this.state.solution.type} Z = {this.state.solution.z}<br/>x1 = {this.state.solution.x}<br/>x2 = {this.state.solution.y}</p>
+            }
+          </li>
           <li className="list-group-item list-group-item-dark">Región de soluciones no factibles</li>
         </ul>
       </div>
